49eb823475c4e261e52189d7753f54fcd2924963
Kevin <86487929+tar83@users.noreply.github.com>
2023-03-29 12:54:01 -0500

    Added: Login functionality

--- website/views.py
+++ website/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {})
+
+def login_user(request):
+    # Check if logging in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # Authenticate
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged in.")
+        else:
+            messages.success(request, "There was an error logging in, try again.")
+
+        return redirect('home')
+
+    return redirect('home')
+
+def logout_user(request):
+    pass
