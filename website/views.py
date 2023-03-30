580c0d52c40e451a4985e24fc9896e05f6fca772
Kevin <86487929+tar83@users.noreply.github.com>
2023-03-30 15:14:31 -0500

    Added: Table at home

--- website/views.py
+++ website/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    records = Record.objects.all()
+    return render(request, 'home.html', {'records':records})
 
 def login_user(request):
     # Check if logging in
