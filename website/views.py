3b066f1ab3a88c265a0e13f0056542f8f66bc843
Kevin <86487929+tar83@users.noreply.github.com>
2023-03-31 08:02:45 -0500

    Added: Add new records functionality

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
 # Create your views here.
@@ -68,4 +68,18 @@ def delete_record(request, pk):
     else:
         messages.success(request, "You must be logged in to do that.")
 
+    return redirect('home')
+
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, "Record added.")
+                return redirect('home')
+
+        return render(request, 'add_record.html', {'form': form})
+
+    messages.success(request, "You must be logged in.")
     return redirect('home')
